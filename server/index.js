5a9e5a279b17fe336a24c7cba7059440a724d924
David Lee <ing.jdplee@gmail.com>
2022-11-23 15:57:09 -0400

    MongoDB Registering, Installation and Setup Step
    - A lot of typo fixes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bodyParser from "body-parse";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -19,7 +19,7 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlenconded({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
@@ -33,3 +33,15 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
